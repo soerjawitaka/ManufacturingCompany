2ac374012d45e2ec367327f33eb8ed08c4974450
soerjawitaka <js190325@southeast.edu>
2017-10-15 15:11:35 -0500

    created generic employee timesheet controller and views

---
+++ ManufacturingCompany/Controllers/EmployeeTimesheetsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ManufacturingCompany.Models;
+
+namespace ManufacturingCompany.Controllers
+{
+    public class EmployeeTimesheetsController : Controller
+    {
+        private BusinessEntities db = new BusinessEntities();
+
+        // GET: EmployeeTimesheets
+        public ActionResult Index()
+        {
+            var timesheets = db.Timesheets.Include(t => t.AspNetUser);
+            return View(timesheets.ToList());
+        }
+
+        // GET: EmployeeTimesheets/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Timesheet timesheet = db.Timesheets.Find(id);
+            if (timesheet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(timesheet);
+        }
+
+        // GET: EmployeeTimesheets/Create
+        public ActionResult Create()
+        {
+            ViewBag.employee_id = new SelectList(db.AspNetUsers, "Id", "Email");
+            return View();
+        }
+
+        // POST: EmployeeTimesheets/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,employee_id,punch_in_time,punch_out_time")] Timesheet timesheet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Timesheets.Add(timesheet);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.employee_id = new SelectList(db.AspNetUsers, "Id", "Email", timesheet.employee_id);
+            return View(timesheet);
+        }
+
+        // GET: EmployeeTimesheets/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Timesheet timesheet = db.Timesheets.Find(id);
+            if (timesheet == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.employee_id = new SelectList(db.AspNetUsers, "Id", "Email", timesheet.employee_id);
+            return View(timesheet);
+        }
+
+        // POST: EmployeeTimesheets/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,employee_id,punch_in_time,punch_out_time")] Timesheet timesheet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(timesheet).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.employee_id = new SelectList(db.AspNetUsers, "Id", "Email", timesheet.employee_id);
+            return View(timesheet);
+        }
+
+        // GET: EmployeeTimesheets/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Timesheet timesheet = db.Timesheets.Find(id);
+            if (timesheet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(timesheet);
+        }
+
+        // POST: EmployeeTimesheets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Timesheet timesheet = db.Timesheets.Find(id);
+            db.Timesheets.Remove(timesheet);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
