759f6c3e8a4ef21114dd7dd7b21dcae758f5445c
soerjawitaka <js190325@southeast.edu>
2017-11-13 09:41:59 -0600

    first new commit try

--- ManufacturingCompany/App_Data/tblProduct_Category.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [dbo].Product_Category
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1001, 1), 
-    [category_name] VARCHAR(50) NOT NULL
-)
--- ManufacturingCompany/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ManufacturingCompany.MvcApplication" Language="C#" %>
