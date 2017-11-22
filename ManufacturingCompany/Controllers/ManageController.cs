756d6a2961ce539f9581980031d320c1391e22e9
soerjawitaka <js190325@southeast.edu>
2017-11-21 19:59:27 -0600

    change password partial

--- ManufacturingCompany/Controllers/ManageController.cs
+++ ManufacturingCompany/Controllers/ManageController.cs
@@ -266,6 +266,37 @@ namespace ManufacturingCompany.Controllers
         }
 
         //
+        // GET: /Manage/ChangePassword
+        public PartialViewResult PartialChangePassword()
+        {
+            return PartialView();
+        }
+
+        //
+        // POST: /Manage/ChangePassword
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> ChangePassword(ChangePasswordViewModel model)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return View(model);
+        //    }
+        //    var result = await UserManager.ChangePasswordAsync(User.Identity.GetUserId(), model.OldPassword, model.NewPassword);
+        //    if (result.Succeeded)
+        //    {
+        //        var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+        //        if (user != null)
+        //        {
+        //            await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+        //        }
+        //        return RedirectToAction("Index", new { Message = ManageMessageId.ChangePasswordSuccess });
+        //    }
+        //    AddErrors(result);
+        //    return View(model);
+        //}
+
+        //
         // GET: /Manage/ChangeProfile
         public ActionResult ChangeProfile()
         {
